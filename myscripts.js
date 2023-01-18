d9ded54f706cc4a884d34665aaa87c900f94f684
Zceri <43930099+Zceri@users.noreply.github.com>
2023-01-18 12:49:04 -0800

    added to bottom navbar

--- myscripts.js
+++ myscripts.js
@@ -13,19 +13,30 @@ document.getElementById("hotdrinks").classList.add('notinuse');
 document.getElementById("colddrinks").classList.add('notinuse');
 document.getElementById("groceryitems").classList.add('notinuse');
 document.getElementById("fees").classList.add('notinuse');
+document.getElementById("merch").classList.add('notinuse');
+document.getElementById("giftcards").classList.add('notinuse');
+document.getElementById("wholesale").classList.add('notinuse');
+
+document.getElementById("secondbottom").classList.add('notinuse');
+
 function purplebottom1(){
   document.getElementById("bottombar1").classList.add('active');
   document.getElementById("bottombar2").classList.remove('active');
   document.getElementById("bottombar3").classList.remove('active');
   document.getElementById("bottombar4").classList.remove('active');
   document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.remove('active');
   
   document.getElementById("food").classList.remove('notinuse');
   document.getElementById("hotdrinks").classList.add('notinuse');
   document.getElementById("colddrinks").classList.add('notinuse');
   document.getElementById("groceryitems").classList.add('notinuse');
   document.getElementById("fees").classList.add('notinuse');
-
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
 }
 
 function purplebottom2(){
@@ -34,12 +45,18 @@ function purplebottom2(){
   document.getElementById("bottombar3").classList.remove('active');
   document.getElementById("bottombar4").classList.remove('active');
   document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.remove('active');
   
   document.getElementById("food").classList.add('notinuse');
   document.getElementById("hotdrinks").classList.remove('notinuse');
   document.getElementById("colddrinks").classList.add('notinuse');
   document.getElementById("groceryitems").classList.add('notinuse');
   document.getElementById("fees").classList.add('notinuse');
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
 }
 
 function purplebottom3(){
@@ -48,12 +65,18 @@ function purplebottom3(){
   document.getElementById("bottombar3").classList.add('active');
   document.getElementById("bottombar4").classList.remove('active');
   document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.remove('active');
   
   document.getElementById("food").classList.add('notinuse');
   document.getElementById("hotdrinks").classList.add('notinuse');
   document.getElementById("colddrinks").classList.remove('notinuse');
   document.getElementById("groceryitems").classList.add('notinuse');
   document.getElementById("fees").classList.add('notinuse');
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
 }
 
 function purplebottom4(){
@@ -62,12 +85,18 @@ function purplebottom4(){
   document.getElementById("bottombar3").classList.remove('active');
   document.getElementById("bottombar4").classList.add('active');
   document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.remove('active');
   
   document.getElementById("food").classList.add('notinuse');
   document.getElementById("hotdrinks").classList.add('notinuse');
   document.getElementById("colddrinks").classList.add('notinuse');
   document.getElementById("groceryitems").classList.remove('notinuse');
   document.getElementById("fees").classList.add('notinuse');
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
 }
 
 function purplebottom5(){
@@ -76,14 +105,90 @@ function purplebottom5(){
   document.getElementById("bottombar3").classList.remove('active');
   document.getElementById("bottombar4").classList.remove('active');
   document.getElementById("bottombar5").classList.add('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.remove('active');
   
   document.getElementById("food").classList.add('notinuse');
   document.getElementById("hotdrinks").classList.add('notinuse');
   document.getElementById("colddrinks").classList.add('notinuse');
   document.getElementById("groceryitems").classList.add('notinuse');
   document.getElementById("fees").classList.remove('notinuse');
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
 }
 
+function purplebottom6(){
+  document.getElementById("bottombar1").classList.remove('active');
+  document.getElementById("bottombar2").classList.remove('active');
+  document.getElementById("bottombar3").classList.remove('active');
+  document.getElementById("bottombar4").classList.remove('active');
+  document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.add('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.remove('active');
+  
+  document.getElementById("food").classList.add('notinuse');
+  document.getElementById("hotdrinks").classList.add('notinuse');
+  document.getElementById("colddrinks").classList.add('notinuse');
+  document.getElementById("groceryitems").classList.add('notinuse');
+  document.getElementById("fees").classList.add('notinuse');
+  document.getElementById("merch").classList.remove('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
+}
+
+function purplebottom7(){
+  document.getElementById("bottombar1").classList.remove('active');
+  document.getElementById("bottombar2").classList.remove('active');
+  document.getElementById("bottombar3").classList.remove('active');
+  document.getElementById("bottombar4").classList.remove('active');
+  document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.add('active');
+  document.getElementById("bottombar8").classList.remove('active');
+  
+  document.getElementById("food").classList.add('notinuse');
+  document.getElementById("hotdrinks").classList.add('notinuse');
+  document.getElementById("colddrinks").classList.add('notinuse');
+  document.getElementById("groceryitems").classList.add('notinuse');
+  document.getElementById("fees").classList.add('notinuse');
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.remove('notinuse');
+  document.getElementById("wholesale").classList.add('notinuse');
+}
+
+function purplebottom8(){
+  document.getElementById("bottombar1").classList.remove('active');
+  document.getElementById("bottombar2").classList.remove('active');
+  document.getElementById("bottombar3").classList.remove('active');
+  document.getElementById("bottombar4").classList.remove('active');
+  document.getElementById("bottombar5").classList.remove('active');
+  document.getElementById("bottombar6").classList.remove('active');
+  document.getElementById("bottombar7").classList.remove('active');
+  document.getElementById("bottombar8").classList.add('active');
+  
+  document.getElementById("food").classList.add('notinuse');
+  document.getElementById("hotdrinks").classList.add('notinuse');
+  document.getElementById("colddrinks").classList.add('notinuse');
+  document.getElementById("groceryitems").classList.add('notinuse');
+  document.getElementById("fees").classList.add('notinuse');
+  document.getElementById("merch").classList.add('notinuse');
+  document.getElementById("giftcards").classList.add('notinuse');
+  document.getElementById("wholesale").classList.remove('notinuse');
+}
+
+
+function graybottom1(){
+  document.getElementById("firstbottom").classList.add('notinuse');
+  document.getElementById("secondbottom").classList.remove('notinuse');
+}
+
+function graybottom2(){
+  document.getElementById("firstbottom").classList.remove('notinuse');
+  document.getElementById("secondbottom").classList.add('notinuse');
+}
 
 function forhereswitch() {
   forhere++;
